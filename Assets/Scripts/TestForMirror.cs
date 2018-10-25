57a4d1e59766406ccaf92ee241cfc4452ced41cb
CLLvGit <1065817791@qq.com>
2018-10-25 20:12:20 +0800

    道具栏相关完成

---
+++ Assets/Scripts/TestForMirror.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestForMirror : MonoBehaviour {
+    public static int TargetNum = 1;
+    public GameObject[] Target = new GameObject[TargetNum];
+	// Use this for initialization
+	void Start () {
+        foreach (GameObject target in Target)
+            if (target == null)
+                Debug.Log("道具的目标位置未设置");
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject == Target[0])
+            this.transform.Rotate(new Vector3(10, 1, 1), Space.Self);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
