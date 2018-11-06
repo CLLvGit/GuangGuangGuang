d97c8f243569c00ca059499066d055c85d1637d0
KaLuLas <kalulas41@gmail.com>
2018-11-07 00:18:46 +0800

    before level_2
    level_1 bug fix
    function flick modified
    level_2 unfinished

---
+++ Assets/Scripts/Level2/PickItem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickItem : MonoBehaviour {
+	public bool TrueItem;
+	public GameObject Item;
+	public GameObject Reaction;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	private void OnMouseDown(){
+		Item.GetComponent<SpriteRenderer>().sprite = null;
+		Item.GetComponent<BoxCollider2D>().enabled = false;
+		GameManager.gm.Block();
+		Invoke("ShowReaction",1f);
+		Invoke("ItemGetGameClear",3f);
+	}
+
+	private void ShowReaction(){
+		Reaction.SetActive(true);
+	}
+
+	private void ItemGetGameClear(){
+		if(TrueItem){
+			GameManager.gm.GameWin();
+		}
+		else{
+			GameManager.gm.GameFaild();
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
