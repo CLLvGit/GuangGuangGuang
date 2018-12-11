ce7390bec81c72f57a0d89ac053311f9e17853e3
CLLvGit <1065817791@qq.com>
2018-12-11 18:10:45 +0800

    第六关完成摆放，拾取部分，筷子部分待完成

---
+++ Assets/Scripts/Level6/ShakeChopstick.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeChopstick : MonoBehaviour {
+    public GameObject[] Tools = new GameObject[3];
+    private int now = 0;
+	// Use this for initialization
+	void Start () {
+        for(int i = 0; i< Tools.Length; i++)
+        {
+            GameObject t = Tools[i];
+            if (t == null)
+                Debug.Log(this.name + "摇晃道具未设置！");
+            if (i != 0)
+                t.SetActive(false);
+        }
+        Tools[now].SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void ShakeOneTime()
+    {
+        Tools[now].SetActive(false);
+        now = (now + 1) % Tools.Length;
+        Tools[now].SetActive(true);
+    }
+    private void Shake()
+    {
+        Invoke("ShakeOneTime", 1);
+        Invoke("ShakeOneTime", 1);
+        Invoke("ShakeOneTime", 1);
+    }
+
+}
---
+++ Assets/Scripts/Level6/ShowSthOnClick.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowSthOnClick : MonoBehaviour {
+    public GameObject[] SthToShow = new GameObject[0];
+    public GameObject[] SthToHide = new GameObject[0];
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < SthToShow.Length; i++)
+        {
+            GameObject sth = SthToShow[i];
+            if (sth == null)
+                Debug.Log(this.name + "未设置需要显示的物品！");
+            sth.SetActive(false);
+        }
+        for (int i = 0; i < SthToHide.Length; i++)
+        {
+            GameObject sth = SthToHide[i];
+            if (sth == null)
+                Debug.Log(this.name + "未设置需要隐藏的物品！");
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnMouseDown()
+    {
+        for (int i = 0; i < SthToShow.Length; i++)
+        {
+            GameObject sth = SthToShow[i];
+            sth.SetActive(true);
+        }
+        for (int i = 0; i < SthToHide.Length; i++)
+        {
+            GameObject sth = SthToHide[i];
+            sth.SetActive(false);
+        }
+        this.gameObject.SetActive(false);
+    }
+}
