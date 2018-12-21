e3dcdbff30e139e33423dbe0f981eb488c5c2605
KaLuLas <kalulas41@gmail.com>
2018-12-22 01:16:38 +0800

    DEBUG & 音效添加

---
+++ Assets/Scripts/Level5/LockedItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+	}
+	
+	public void Unlocked(){
+		this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
