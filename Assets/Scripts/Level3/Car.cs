d4a867055c7a6cf4c11714c3b5191e284d6a45cd
bao zhenyu <16307130050@fudan.edu.cn>
2018-12-23 00:34:19 +0800

    第三关添加彩蛋
    第三关添加彩蛋：印度汽车
    修改了鞋柜开门音效

--- Assets/Scripts/Level3/Car.cs
+++ Assets/Scripts/Level3/Car.cs
@@ -6,6 +6,7 @@ public class Car : MonoBehaviour {
     public GameObject Start_point;
     public GameObject End_point;
     public GameObject Moving_car;
+    public GameObject Indian_car;
     public float delta_x, delta_y;
     public Vector3 Start_position, End_position, Car_position;
     public float interval;
@@ -15,6 +16,7 @@ public class Car : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        Indian_car.SetActive(false);
         is_moving = true;
         Start_position = Start_point.transform.position;
         Moving_car.transform.position = Start_position;
@@ -57,6 +59,11 @@ public class Car : MonoBehaviour {
                 {
                     Moving_car.transform.position = Start_position;
                     Moving_car.GetComponent<AudioSource>().Play();
+                    int x = Random.Range(0, 9);
+                    if (x == 5)
+                        Indian_car.SetActive(true);
+                    else
+                        Indian_car.SetActive(false);
                     is_moving = true;
                 }
             }
