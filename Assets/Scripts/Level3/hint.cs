1a941f75587e2a056d73cec3c5e9042e0c9bbcb4
bao zhenyu <16307130050@fudan.edu.cn>
2018-12-23 13:04:38 +0800

    真·最后一版
    第三关添加提示
    第三关室外汽车碰撞体积增加
    更改了过关音效

---
+++ Assets/Scripts/Level3/hint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hint : MonoBehaviour {
+    public GameObject roadblock_hint, tele_on, tele_off;
+    bool hint_enabled = false;
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = Time.time;
+        roadblock_hint.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float current_time = Time.time;
+        if (current_time - timer > 30f)
+        {
+            tele_off.SetActive(false);
+            tele_on.SetActive(true);
+            roadblock_hint.SetActive(true);
+        }
+	}
+}
